ca39350d271d61c5857821c9a8a4dedfb0774a4e
Israel Akintunde <israelakintunde005@gmail.com>
2019-08-28 22:24:36 +0100

    added Github API endpoint

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,6 +1,7 @@
 'use strict';
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { fetchPopularRepos } from '../utils/api';
 
 function LanguagesNav({selected, onUpdateLanguage}) {
   const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python','C','Go','Rust'];
@@ -30,20 +31,52 @@ export default class Popular extends Component {
     super(props);
     
     this.state = {
-      selectedLanguage: 'All'
+      selectedLanguage: 'All',
+      repos: null,
+      error: null
     };
     
     this.updateLanguage = this.updateLanguage.bind(this);
+    this.isLoading = this.isLoading.bind(this);
+  }
+  
+  componentDidMount() {
+    this.updateLanguage(this.state.selectedLanguage);
   }
   
   updateLanguage(selectedLanguage) {
-    this.setState({selectedLanguage})
+    this.setState({
+      selectedLanguage,
+      repos: null,
+      error: null
+    });
+    
+    fetchPopularRepos(selectedLanguage)
+      .then(repos => this.setState({
+        repos,
+        error: null
+      }))
+      .catch(error => {
+        console.error(`There was an error fetching the repositories ${error}`);
+        
+        this.setState({error: 'There was an error fetching the repositories'})
+      })
   }
+  
+  isLoading() {
+    return this.state.repos === null && this.state.error === null;
+  }
+  
   render() {
-    let { selectedLanguage } = this.state;
+    let { selectedLanguage, repos, error } = this.state;
     return (
       <React.Fragment>
         <LanguagesNav onUpdateLanguage={this.updateLanguage} selected={selectedLanguage}/>
+        {this.isLoading() && <p>LOADING</p>}
+        
+        {error && <p>{error}</p>}
+        
+        {repos && <pre>{JSON.stringify(repos, null, 2)}</pre>}
       </React.Fragment>
     )
   }
