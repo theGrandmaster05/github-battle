766b57f413309c74503cc5ba676e198c1a6a1acb
Israel Akintunde <israelakintunde005@gmail.com>
2019-08-29 00:56:06 +0100

    Started Battle

--- app/App.js
+++ app/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Popular from './components/Popular';
+import Battle from './components/Battle';
 
 export default class App extends Component {
   render() {
     return (
       <div className="container">
-        <Popular/>
+        {/*<Popular/>*/}
+        <Battle/>
       </div>
     )
   }
--- app/components/Battle.js
+++ app/components/Battle.js
@@ -11,10 +11,24 @@ function Instructions () {
         <li>
           <h3 className="header-sm">
             Enter two GitHub users
-            <FaUserFriends color='#D94A38' size={18} className='bg-light'/>
+            <FaUserFriends color='#D94A38' size={140} className='bg-light'/>
           </h3>
+        </li>
+        <li>
+          <h3 className="header-sm">See the winners</h3>
+          <FaTrophy size={140} color='#D94A38' className='bg-light'/>
         </li>
       </ol>
     </div>
   )
+}
+
+export default class Battle extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Instructions/>
+      </React.Fragment>
+    )
+  }
 }
