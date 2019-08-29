ca5d81694901676dd92711038817b4aa3c0de173
Israel Akintunde <israelakintunde005@gmail.com>
2019-08-29 22:23:38 +0100

    Formatted component to use HOC

---
+++ app/components/withHover.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+export default function withHover(Component, propName = "hovering") {
+  return class withHover extends Component {
+    constructor(props) {
+      super(props);
+      
+      this.state = {
+        hovering: false
+      };
+      
+      this.mouseOver = this.mouseOver.bind(this);
+      this.mouseOut = this.mouseOut.bind(this);
+    }
+    
+    mouseOver() {
+      this.setState({hovering: true});
+    }
+    
+    mouseOut() {
+      this.setState({hovering: true});
+    }
+    
+    render() {
+      const props = {
+        [propName]: this.state.hovering,
+        ...this.props
+      };
+      
+      return (
+        <div onMouseOver={this.mouseOver} onMouseOut={this.mouseOut}>
+          <Component {...props}/>
+        </div>
+      )
+    }
+  }
+}
